a7b5b1424ecdf3a2aa0db30174044bd1fedf5ed3
detronetdip <ayondip2001@gmail.com>
2022-03-06 07:50:30 +0530

    analytics added Sun  6 Mar 07:50:30 IST 2022

--- d3h45sew5-t5r6-uy-io/Admin/index.php
+++ d3h45sew5-t5r6-uy-io/Admin/index.php
@@ -118,7 +118,7 @@ authenticate();
 <!-- analytics -->
 <div class="selectionrow">
     <div class="wrap">
-        <span>Updated on 01/03/2022 at 23:09</span>
+        <span>Updated on 06/03/2022 at 07:09</span>
         <span>Free till 06/05/2020</span>
     </div>
     <div class="sectbord" style="background-color:transparent;">
@@ -148,7 +148,7 @@ authenticate();
         </div>
         <div class="card pgs_" onclick="viewPs_()">
             <div class="image">
-                <span id="ttlps">5.9</span>
+                <span id="ttlps">6.6</span>
             </div>
             <div class="write">
                 <span>Position</span>
